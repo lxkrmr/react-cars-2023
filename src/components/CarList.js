1e8c39775883e7ee1f0db9288774ca212b4fcdbe
Alex <lxkrmr@icloud.com>
2023-02-21 10:59:30 +0100

    ✨ show cars in CarList

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCar } from '../store';
+
 function CarList() {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleCarDelete(car)}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
